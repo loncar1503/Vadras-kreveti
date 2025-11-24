f0b63b05002cfcb1dbe396868fa3b7b6f2cefa7f
loncardjordje <loncardjordje1503@gmail.com>
2025-11-24 20:37:45 +0100

    Domain gotov

---
+++ Domain/Radnik.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Radnik
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; } = null!;
+        public string Sifra { get; set; } = null!;     
+
+        public string ImePrezime { get; set; } = null!;
+
+        public List<Porudzbina> Porudzbine { get; set; } = new();
+    }
+}
